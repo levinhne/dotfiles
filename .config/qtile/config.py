6f4a7289bc0fe77573d02de0ea6898b93ab8a909
levinhne <levinhne@gmail.com>
2024-03-06 17:34:37 +0700

    update qtile, vim

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -69,7 +69,7 @@ keys = [
     ),
     Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
     # Toggle between different layouts as defined below
-    # Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
+    Key([mod], "Space", lazy.next_layout(), desc="Toggle between layouts"),
     Key([mod], "w", lazy.window.kill(), desc="Kill focused window"),
     Key(
         [mod],
