32a202f9365b1e0bd3eefecc6d56f168d44c62ba
Le Quang Vinh <levinhne@gmail.com>
2024-05-04 11:07:31 +0700

    update alias

--- .config/fish/conf.d/alias.fish
+++ .config/fish/conf.d/alias.fish
@@ -1,5 +1,3 @@
-alias cd='z'
-
 alias ls='eza -al --color=always --group-directories-first' # my preferred listing
 alias la='eza -a --color=always --group-directories-first'  # all files and dirs
 alias ll='eza -l --color=always --group-directories-first'  # long format
