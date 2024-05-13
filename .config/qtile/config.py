877decae6058085903e0e6257ae71d215d59ee0f
Vinh Quang Le <vinhlq@smartosc.com>
2024-05-13 16:35:11 +0700

    update nvim

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -401,9 +401,9 @@ floating_layout = layout.Floating(
         Match(wm_class="makebranch"),  # gitk
         Match(wm_class="maketag"),  # gitk
         Match(wm_class="ssh-askpass"),  # ssh-askpass
+        Match(wm_class="Pcmanfm"),
         Match(title="branchdialog"),  # gitk
         Match(title="pinentry"),  # GPG key password entry
-        Match(title="pcmanfm"),  # GPG key password entry
     ],
 )
 auto_fullscreen = True
