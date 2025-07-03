fd9463b9b7bf3f9c3fb58c1b67107d6522c26158
vinhlq21 <vinhlq21@fpt.com>
2025-07-03 15:18:19 +0700

    update nvim

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -15,6 +15,8 @@ vim.opt.mouse = ""
 
 -- map({ "n", "i", "v" }, "<leader>gg", "<cmd>LazyGit<cr>", { desc = "lazygit" })
 
+map("n", "<leader>e", ":Telescope file_browser<CR>")
+
 map("n", "<leader>lr", "<cmd>LspRestart<cr>", { desc = "lsp restart" })
 
 map("i", "<C-CR>", 'copilot#Accept("\\<CR>")', { expr = true, replace_keycodes = false })
