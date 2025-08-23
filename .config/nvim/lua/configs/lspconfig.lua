d8f3294b012cc8551aa7c016645b2553f7a59ebc
Le Quang Vinh <levinhne@gmail.com>
2025-08-24 00:10:00 +0700

    update kitty nvim lsp

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -17,22 +17,6 @@ local servers = {
   "tailwindcss",
 }
 
-vim.lsp.config("gopls", {
-  cmd = { "gopls" },
-  filetypes = { "go", "gomod", "gowork", "gotmpl", "tmpl" },
-  root_dir = util.root_pattern("go.work", "go.mod"),
-  settings = {
-    gopls = {
-      buildFlags = { "-tags=wireinject" },
-      completeUnimported = true,
-      usePlaceholders = true,
-      analyses = {
-        unusedparams = true,
-      },
-    },
-  },
-})
-
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers
