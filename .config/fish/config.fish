85be7c476d54bbaa2e61e0066aead1b12fee9bb1
Le Quang Vinh <4110288+levinhne@users.noreply.github.com>
2025-12-24 00:40:12 +0700

    Update fish_greeting function in config.fish
    Define a function for fish greeting and add interactive session commands.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,2 +1,8 @@
-set fish_greeting
+function fish_greeting
+end
+
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
 fish_config theme choose "Dracula Official"
